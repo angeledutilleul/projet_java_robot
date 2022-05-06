dea3c207655bebdccce99707fd940e7fa1da5c49
angel <angele.dutilleul@gmail.com>
2022-05-06 16:13:36 +0200

    if

--- src/main/java/isep/jfx/MainApplication.java
+++ src/main/java/isep/jfx/MainApplication.java
@@ -18,7 +18,7 @@ public class MainApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Ricochet Robots");
         stage.setScene(scene);
-        stage.show();
+        stage.show(); if ()
     }
 
 }
